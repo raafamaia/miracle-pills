7a7a97c12cb8cf3539e5a732c3e6229d37b9a216
Maia <raafamaia@gmail.com>
2016-10-10 22:35:03 -0300

    finished

--- miracle-pills/ViewController.swift
+++ miracle-pills/ViewController.swift
@@ -8,18 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var btnStatePicker: UIButton!
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var lblCountry: UILabel!
+    @IBOutlet weak var txtFieldCOuntry: UITextField!
+    @IBOutlet weak var lblZip: UILabel!
+    @IBOutlet weak var txtFieldZip: UITextField!
+    @IBOutlet weak var imgSucces: UIImageView!
+    
+    let states = ["São Paulo", "Rio de Janeiro", "Acre", "Minas Gerais"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        statePicker.delegate = self
+        statePicker.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func btnChooseStatePressed(_ sender: AnyObject) {
+        toggle(isHidden: true)
+        
     }
-
-
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        btnStatePicker.setTitle(states[row], for: UIControlState.normal)
+        toggle(isHidden: false)
+    }
+    
+    func toggle(isHidden: Bool) {
+        statePicker.isHidden = !isHidden
+        lblCountry.isHidden = isHidden
+        txtFieldCOuntry.isHidden = isHidden
+        lblZip.isHidden = isHidden
+        txtFieldZip.isHidden = isHidden
+    }
+    @IBAction func btnBuyNowPressed(_ sender: AnyObject) {
+        imgSucces.isHidden = false
+    }
 }
 
